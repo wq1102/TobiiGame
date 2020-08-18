d101f8b6424ca00c571ffa89651cd10c1dc154f0
wq1102 <45382509+wq1102@users.noreply.github.com>
2020-08-18 14:39:09 +0800

    Add files via upload

---
+++ TobiiGame/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace TobiiGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.axShockwaveFlash1 = new AxShockwaveFlashObjects.AxShockwaveFlash();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.Font = new System.Drawing.Font("宋体", 12F);
+            this.button1.Location = new System.Drawing.Point(446, 685);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(362, 53);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start Game";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // axShockwaveFlash1
+            // 
+            this.axShockwaveFlash1.Enabled = true;
+            this.axShockwaveFlash1.Location = new System.Drawing.Point(293, 12);
+            this.axShockwaveFlash1.Name = "axShockwaveFlash1";
+            this.axShockwaveFlash1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axShockwaveFlash1.OcxState")));
+            this.axShockwaveFlash1.Size = new System.Drawing.Size(689, 612);
+            this.axShockwaveFlash1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.YellowGreen;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("黑体", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(12, 278);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(249, 194);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "点击Start Game开始游戏\r\n\r\n\r\n游戏规则：\r\n    从左上角开始，眼睛每转动一圈（顺时针逆时针皆可），小兔子就会拔一个萝卜，每个萝卜记5分，满分10" +
+    "0分";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.YellowGreen;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("黑体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox2.Location = new System.Drawing.Point(1092, 306);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(134, 34);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "得分：";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.BackColor = System.Drawing.Color.YellowGreen;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Font = new System.Drawing.Font("黑体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox3.ForeColor = System.Drawing.Color.Red;
+            this.textBox3.Location = new System.Drawing.Point(1092, 398);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(134, 47);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "0";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1300, 750);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.axShockwaveFlash1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axShockwaveFlash1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private AxShockwaveFlashObjects.AxShockwaveFlash axShockwaveFlash1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ TobiiGame/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tobii.Interaction;
+using Tobii.Interaction.Framework;
+using System.Runtime.InteropServices;
+
+
+namespace TobiiGame
+{
+    
+    public partial class Form1 : Form
+
+    {
+        int score = 0;
+        int n = 0;
+        List<int> Position = new List<int>();
+        public Form1()
+        {
+            InitializeComponent();
+            this.BackgroundImage = Image.FromFile("D:\\TobiiGame\\green.jpg");
+        }
+
+      
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool flag = false;
+            var host = new Host();
+            int[,] CenterPoint = new int[,] { { Screen.PrimaryScreen.WorkingArea.Width / 2, Screen.PrimaryScreen.WorkingArea.Height / 2 } };
+            int P;
+            Position.Add(0);
+            var gazePointDataStream = host.Streams.CreateGazePointDataStream(Tobii.Interaction.Framework.GazePointDataMode.LightlyFiltered);
+            axShockwaveFlash1.Movie = "D:\\rabbit.swf";
+            axShockwaveFlash1.Rewind();
+            axShockwaveFlash1.StopPlay();
+            gazePointDataStream.GazePoint((x, y, _) =>
+            {
+                if (flag)
+                {
+                    this.axShockwaveFlash1.Play();
+                    System.Timers.Timer t = new System.Timers.Timer(3000);
+                    t.Elapsed += new System.Timers.ElapsedEventHandler(theout);//到达时间的时候执行事件；
+                    t.AutoReset = false;//设置是执行一次（false）还是一直执行(true)；
+                    t.Enabled = true;
+                    flag = false;
+                    
+                }
+                else
+                {
+                    if (x >= 0 && y >= 0)
+                    {
+                        if (x <= CenterPoint[0, 0] && y <= CenterPoint[0, 1])
+                        {
+                            P = 0;
+                        }
+                        else if (x > CenterPoint[0, 0] && y <= CenterPoint[0, 1])
+                        {
+                            P = 1;
+                        }
+                        else if (x > CenterPoint[0, 0] && y > CenterPoint[0, 1])
+                        {
+                            P = 2;
+                        }
+                        else
+                        {
+                            P = 3;
+                        }
+                    
+                        if (Position[n] != P)
+                        {
+                            Position.Add(P);
+                            n += 1;
+                            
+                            if (Position.Count() == 5)
+                            {
+                                //Console.WriteLine("{0},{1},{2},{3}", Position[0].ToString(), Position[1].ToString(), Position[2].ToString(), Position[3].ToString());
+                                if ((Position[0] == 0 && Position[1] == 1 && Position[2] == 2 && Position[3] == 3 && Position[4] == 0) || (Position[0] == 0 && Position[1] == 3 && Position[2] == 2 && Position[3] == 1 && Position[4] == 0))
+                                {
+                                    Position.Clear();
+                                    Position.Add(0);
+                                    flag = true;
+                                    n = 0;
+                                }
+                                else
+                                {
+
+
+                                    MessageBox.Show("请从左上角重新开始,3s后开始", "提示");
+
+                                    System.Timers.Timer t1 = new System.Timers.Timer(3000);
+                                    t1.Elapsed += new System.Timers.ElapsedEventHandler(KillMessageBox);
+                                    t1.AutoReset = false;//设置是执行一次（false）还是一直执行(true)；
+                                    t1.Enabled = true;
+                                    //Console.WriteLine("失败：{0},{1},{2},{3},{4}", Position[0].ToString(), Position[1].ToString(), Position[2].ToString(), Position[3].ToString(), Position[4].ToString());
+                                    
+
+
+
+                                }
+                            }
+            
+                        }
+                    }
+                    
+
+
+
+
+                }
+            });
+            
+
+
+
+
+
+        }
+        public void theout(object source, System.Timers.ElapsedEventArgs e)
+
+        {
+            this.axShockwaveFlash1.StopPlay();
+            score += 5;
+            textBox3.Invoke((MethodInvoker)(() => textBox3.Text = score.ToString()));
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox3_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+        private void KillMessageBox(object source, System.Timers.ElapsedEventArgs e)
+        {
+          
+            Position.Clear();
+            Position.Add(0);
+            n = 0;
+        }
+    }
+}
